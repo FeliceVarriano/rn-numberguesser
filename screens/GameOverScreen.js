168eb8fe698e9f413f7c64eafd8ab8cb026cadec
Felice Varriano <felice.varriano@nserc-crsng.gc.ca>
2021-12-04 10:28:53 -0500

    additional styling and images

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -5,14 +5,24 @@ import {
   Text,
   View,
   Button,
+  Image,
 } from "react-native";
+import BodyText from "../components/BodyText";
+import HeaderText from "../components/HeaderText";
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The game is over!</Text>
-      <Text>Number of Rounds: {props.roundsNumber}</Text>
-      <Text>Number was: {props.userNumber}</Text>
+      <HeaderText>The game is over!</HeaderText>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          resizeMode="cover"
+          source={require("../assets/success.png")}
+        />
+      </View>
+      <BodyText>Number of Rounds: {props.roundsNumber}</BodyText>
+      <BodyText>Number was: {props.userNumber}</BodyText>
       <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
   );
@@ -25,5 +35,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  imageContainer: {
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    width: 300,
+    height: 300,
+    overflow: "hidden",
+    marginVertical: 30,
   },
 });
